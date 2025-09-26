ba677654de2b5d5c01edea323b976c6f56f77aa4
gianella lastra <gianellalastra4@gmail.com>
2025-09-26 12:06:07 -0300

    Inicio del proyecto base.

---
+++ mi-portafolio/src/components/header.tsx
@@ -0,0 +1,10 @@
+export const Header = (() => {
+    return(
+        <>
+        <h1>Gianella Carla Lastra</h1>
+        <h3>Desarrolladora Full Stack junior</h3>
+        <p>Soy Giane, desarrolladora web junior. Me especializo en construir interfaces simples y atractivas que ayudan a los usuarios a disfrutar de la tecnología sin complicaciones.</p>
+        
+        </>
+    )
+})
